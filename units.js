04a4fe056ec2cd75f3203d272bb98108384321e7
David Janes <davidjanes@davidjanes.com>
2015-07-20 15:22:17 -0400

    use Node IOTDB's conversion library

--- units.js
+++ units.js
@@ -28,49 +28,6 @@ var iotdb = require('iotdb')
 var _ = iotdb._;
 var typed = require('./typed');
 
-var _precision = function(value, source) {
-    source = "" + source;
-
-    var p = 0;
-    var dotx = source.indexOf('.')
-    if (dotx !== -1) {
-        p = source.length - p + 1;
-    }
-
-    return parseFloat(value.toFixed(Math.max(p, 3)));
-}
-
-var conversions = [
-    {
-        from: 'iot-unit:temperature.imperial.fahrenheit',
-        to: 'iot-unit:temperature.si.celsius',
-        convert: function(v) {
-            return _precision(( v - 32 ) / 1.8, v);
-        },
-    },
-    {
-        from: 'iot-unit:temperature.si.celsius',
-        to: 'iot-unit:temperature.imperial.fahrenheit',
-        convert: function(v) {
-            return _precision(v * 1.8 + 32, v);
-        },
-    },
-    {
-        from: 'iot-unit:temperature.si.celsius',
-        to: 'iot-unit:temperature.si.kelvin',
-        convert: function(v) {
-            return v + 273.15;
-        },
-    },
-    {
-        from: 'iot-unit:temperature.si.kelvin',
-        to: 'iot-unit:temperature.si.celsius',
-        convert: function(v) {
-            return v - 273.15;
-        },
-    },
-];
-
 /**
  */
 var units = function(paramd) {
@@ -81,52 +38,43 @@ var units = function(paramd) {
         if (typed.is.Scalar(value)) {
             return new typed.Typed(typed.value(value), value_units);
         } else {
-            return new typed.Typed(convert(typed.units(value), value_units, typed.value(value)), value_units);
+            var nscalar = _.convert.convert({
+                from: typed.units(value), 
+                to: value_units,
+                value: typed.value(value),
+            });
+            return new typed.Typed(nscalar, value_units);
         }
     } else if (typed.is.Array(value)) {
         if (typed.is.Scalar(value)) {
-            var nvalues = [];
+            var nscalars = [];
             typed.value(value).map(function(v) {
                 var nvalue = units({
                     first: v,
                     av: [ v, value_units ],
                     ad: {},
                 });
-                nvalues.push(nvalue.value);
+                nscalars.push(nvalue.value);
             });
-            return new typed.Typed(nvalues, value_units);
+            return new typed.Typed(nscalars, value_units);
         } else {
-            var nvalues = [];
+            var nscalars = [];
             typed.value(value).map(function(v) {
-                var nv = convert(typed.units(value), value_units, v);
-                nvalues.push(nv);
+                var nscalar = _.convert.convert({
+                    from: typed.units(value), 
+                    to: value_units,
+                    value: v,
+                });
+                nscalars.push(nscalar);
             });
-            return new typed.Typed(nvalues, value_units);
+            return new typed.Typed(nscalars, value_units);
         }
     } else {
         return value;
     }
 };
 
-var convert = function(from, to, value) {
-    if (from === to) {
-        return value;
-    }
-
-    for (var ci in conversions) {
-        var cd = conversions[ci];
-        if ((cd.from !== from) || (cd.to !== to)) {
-            continue;
-        }
-
-        return cd.convert(value);
-    }
-
-    throw new Error("no conversion found from '" + from + "' to '" + to + "'");
-};
-
 /**
  *  API
  */
 exports.units = units;
-exports.conversions = conversions;
