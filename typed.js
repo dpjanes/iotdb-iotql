27863a149f66ec0058a57f88e79b086b0039ea7d
David Janes <davidjanes@davidjanes.com>
2015-07-20 07:50:52 -0400

    typed values

---
+++ typed.js
@@ -0,0 +1,62 @@
+/*
+ *  typed.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2015-07-20
+ *
+ *  Copyright [2013-2015] [David P. Janes]
+ *
+ *  This is for handling values that may have a unit and/or scale
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+var iotdb = require('iotdb')
+var _ = iotdb._;
+
+var Typed = function(value, unit) {
+    var self = this;
+
+    this.value = value;
+    this.unit = (unit === undefined) : null : unit;
+    this.scale = 1;     // for future use
+};
+
+Typed.prototype._isTyped = true;
+
+
+var value = function(v) {
+    if (v && v._isTyped) {
+        return v.value * v.scale;
+    } else {
+        return v;
+    }
+};
+
+var unit = function(v) {
+    if (v && v._isTyped) {
+        return v.unit;
+    } else {
+        return null;
+    }
+}
+
+/**
+ *  API
+ */
+exports.Typed = Typed;
+exports.value = value;
+exports.unit = unit;
